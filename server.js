e19b1fec53c2c67707c541b5b5242ba80d7005f0
Je11yBeam <krittaphat.k64@rsu.ac.th>
2023-04-28 14:13:19 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "157.245.59.56",
+  user: "u6400898",
+  password: "6400898",
+  database: "u6400898_hrsensor",
+  port: 3366,
+});
+
+var app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.json({
+    status: "ok",
+    message: "Hello World",
+  });
+});
+
+app.get("/getrate", function (req, res) {
+  connection.query("SELECT * FROM heartrate", function (err, results) {
+    console.log(results); //แสดงผลที่ console
+    res.json(results); //ตอบกลับ request
+  });
+});
+
+// app.get("/pets", function (req, res) {
+//   connection.query(
+//     `INSERT INTO heartrate;`,
+//     function (err, results) {
+//       res.json(results);
+//     }
+//   );
+// });
+
+app.post("/getrate", function (req, res) {
+  const h_data = req.body.h_data;
+  connection.query(
+    `INSERT INTO heartrate (h_data) VALUES (?)`,
+    [h_data],
+    function (err, results) {
+      if (err) {
+        res.json(err);
+      }
+      res.json(results);
+    }
+  );
+});
+
+// app.get("/pets_price", function (req, res) {
+//   connection.query(
+//     `SELECT id, petName, price
+//      FROM pet
+//      ORDER BY price;`,
+//     function (err, results) {
+//       res.json(results);
+//     }
+//   );
+// });
+
+// app.get("/pets_price_chart", function (req, res) {
+//   connection.query(
+//     `SELECT id, petName, price
+//      FROM pet
+//      ORDER BY price;`,
+//     function (err, results) {
+//       const petNames = [];
+//       const prices = [];
+//       for (let i = 0; i < results.length; i++) {
+//         petNames.push(results[i]["petName"]);
+//         prices.push(parseFloat(results[i]["price"]));
+//       }
+//       res.json({
+//         petNames,
+//         prices,
+//       });
+//     }
+//   );
+// });
+
+app.listen(5000, () => {
+  console.log("Server is started.");
+});
